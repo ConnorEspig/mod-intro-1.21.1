102c99024992bcc2b915f71e85407478a12cd7a7
Connor Espig <connorespig@icloud.com>
2024-09-23 20:43:37 -0700

    Project template files

---
+++ src/main/java/com/connorespig/modintro/ModintroDataGenerator.java
@@ -0,0 +1,11 @@
+package com.connorespig.modintro;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModintroDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
